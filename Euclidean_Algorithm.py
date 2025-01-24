bb495396d033f7e005070e1e77e68ee08bc9fbd9
Hadeelalbalushi <3075042A@student.gla.ac.uk>
2025-01-24 17:45:48 +0000

    The code after refacting it to accept the user input and show the error message

--- Euclidean_Algorithm.py
+++ Euclidean_Algorithm.py
@@ -4,13 +4,14 @@ Created on thu Jan 22 00:42:10 2025
 
 @author: hadee
 """
-# creating two global values with a positive integer numbers that we will use to find their GCD
-number1 = 55
-number2 = 20
+ 
+
+
 class Euclidean_Algorithm: 
     """ define a class called Euclidean_Algorithm to encapsulate it """
     
     def calculating_GCD(number1, number2):
+    
         """ defining a method that calculate the 
         greatest common divisor  (GCD) 
         with number1 parameter and number2 parameter
@@ -22,8 +23,26 @@ class Euclidean_Algorithm:
             number2 = remainder# updating number2 value with a value ofremainder 
         return number1 
     """ when the loop ends, it return number1 as the GCD """
-    
-GCD = Euclidean_Algorithm.calculating_GCD(number1, number2)
-"""calling the calculating_GCD method and passing number1 and number2 as arguments"""
-# printing the result 
-print(f"the greatest common divisor of {number1} and {number2} is {GCD}")
+   
+
+try :
+   # The two numbers that we want to get their GCD
+   number1 = int(input("please enter the first positive integer:"))#asking the user to enter the first number
+   number2 = int(input("please enter the second positive integer:"))#asking the user to enter the second number
+   
+   # using the if Condition to check if the user input are  positive integer numbers
+   #so if the user input is less than or equal to zero 
+   if number1<= 0 or number2<= 0 :
+       # so if the input numbers are negative then it will raise(appear) this message 
+           raise ValueError("Both numbers must be positive integers!!")
+
+   GCD = Euclidean_Algorithm.calculating_GCD(number1, number2)
+   """calling the calculating_GCD method and passing number1 and number2 as arguments"""
+    # printing the result 
+   print(f"the greatest common divisor of {number1} and {number2} is {GCD}")
+#this message will appear if the user entered invalid input, and also the program will stop       
+except ValueError as e:
+    print(f"\nInvalid input:{e}")
+    print("\nThis program is stopped because of the invalid input ")
+   
+     
